c570188cabdbaf1184d75868282b697dd5079f61
MamunOfficeLaptop <mamun166036@gmail.com>
2025-02-10 15:42:06 +0600

    up

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import ScrollToTop from "./components/ui/scrolltotop.tsx";
 import React from "react";
 
-//=======================
+//=========================
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import Dashboard from "./app/dashboard/dashboard";
