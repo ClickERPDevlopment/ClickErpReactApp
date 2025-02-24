bd7010829e86fbc59c3235c013a892dd0de2666e
Md. Aburayhan <aburaihan4604@gmail.com>
2025-02-24 16:31:49 +0600

    FabricReceiveReturnChallanGatePassReport Update

--- src/routes/store/finish-fabric-store-routes.tsx
+++ src/routes/store/finish-fabric-store-routes.tsx
@@ -1,6 +1,7 @@
 import OrderWiseFinishFabricDeliveryReport from "@/reports/store/finish-store/order-wise-finish-fabric-delivery-report/order-wise-ff-delivery-report-index";
 import DyeingBillChallanWiseSummaryIndex from "@/reports/store/finish-store/dyeing-bill-challan-wise-summary/dyeing-bill-challan-wise-summary-index";
 import DyeingBillChallanWiseSummaryView from "@/reports/store/finish-store/dyeing-bill-challan-wise-summary/dyeing-bill-challan-wise-summary-view";
+import FabricReceiveReturnChallanGatePassReport from "@/reports/store/finish-store/fabric-receive-return-challan-gate-pass-report/fabric-receive-return-challan-gate-pass-report-index";
 
 export default function FinishFabricStoreRoute(): {
   path: string;
@@ -18,6 +19,10 @@ export default function FinishFabricStoreRoute(): {
     {
       path: "dyeing-bill-challan-wise-summary-index",
       component: <DyeingBillChallanWiseSummaryIndex />,
+    },
+    {
+      path: "fabric-receive-return-challan-gate-pass-report",
+      component: <FabricReceiveReturnChallanGatePassReport />,
     },
   ];
   return routes;
