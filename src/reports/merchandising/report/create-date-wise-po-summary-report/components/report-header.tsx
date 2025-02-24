a819ed819838defcb1391f11cd643c3862422da5
Md. Aburayhan <aburaihan4604@gmail.com>
2025-02-24 09:48:07 +0600

    create date wise po summary report update

--- src/reports/merchandising/report/create-date-wise-po-summary-report/components/report-header.tsx
+++ src/reports/merchandising/report/create-date-wise-po-summary-report/components/report-header.tsx
@@ -10,10 +10,6 @@ function ReportHeader({
       <p className="font-bold text-lg text-left w-[100%] text-sm">
         {moment().format("DD-MMM-YYYY")}
       </p>
-      {/* <h1 className="font-bold text-2xl text-center">{data?.COMPANY_NAME}</h1>
-      <h4 className="font-bold text-base text-center">
-        {data?.COMPANY_ADDRESS}
-      </h4> */}
       <h3 className="font-bold text-xl text-center mt-2">
         Create Date Wise PO Summary Report
       </h3>
