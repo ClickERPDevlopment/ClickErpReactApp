0d604a7cb81be941a688b4ba1781d2de8b6c9341
MamunOfficeLaptop <mamun166036@gmail.com>
2025-03-25 15:26:41 +0600

    Update Notifications component to use new SignalR endpoint and handle connected users

--- src/pages/notifications.tsx
+++ src/pages/notifications.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import * as signalR from "@microsoft/signalr";
 
+export type ConnectedUser = {
+  ConnectionId: string;
+  UserName: string;
+  FullName: string;
+  Designation: string;
+};
+
 export default function Notifications() {
   const [connection, setConnection] = React.useState<signalR.HubConnection>();
   //   const [notification, setNotification] = React.useState(false);
 
   React.useEffect(() => {
     const newConnection = new signalR.HubConnectionBuilder()
-      .withUrl("http://localhost:25635/notificationhub", {
+      .withUrl("http://119.148.62.103:6503/notificationhub", {
         withCredentials: false,
       })
       .withAutomaticReconnect()
@@ -20,16 +27,23 @@ export default function Notifications() {
     if (connection) {
       connection.start().then(() => {
         connection
-          .invoke("GetConnectionId")
-          .then((connectionId: string) =>
-            console.log("connectionId: ", connectionId)
+          .invoke("GetWindowsConnectedClient")
+          .then((lstConnectedUser: ConnectedUser[]) =>
+            console.log("GetWindowsConnectedClient: ", lstConnectedUser)
           );
-        console.log("connected");
+        // console.log("connected");
 
         //---------------
-        connection.on("UserConnected", (message: string) => {
-          console.log("message: ", message);
-        });
+        // connection.on("UserConnected", (message: string) => {
+        //   console.log("UserConnected: ", message);
+        // });
+
+        connection.on(
+          "WindowsConnectedClientStatus",
+          (lstConnectedUser: ConnectedUser[]) => {
+            console.log("WindowsConnectedClientStatus: ", lstConnectedUser);
+          }
+        );
       });
     }
   }, [connection]);
