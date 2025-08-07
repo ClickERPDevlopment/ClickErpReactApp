d3c4d6c48bdcaf732b95efdd2c4dd676edc4e861
raihan50017 <aburaihan50017@gmail.com>
2025-08-07 15:31:51 +0600

    Daily Sewing Efficiency Report

--- src/reports/production/sewing/daily-sewing-efficiency-report/components/report.tsx
+++ src/reports/production/sewing/daily-sewing-efficiency-report/components/report.tsx
@@ -117,7 +117,7 @@ function Report({
           searchParamsObj={searchParamsObj}
         />
         <table className="border-collapse border border-gray-300  w-[100%] mt-3">
-          <thead className="sticky top-0 print:static print:bg-transparent">
+          <thead className="sticky top-0 print:static">
             <tr style={{ fontSize: "15px", backgroundColor: "#A7F3D0" }} className="text-center">
               {firstHeader?.map((item) =>
                 <th className="border border-gray-950 p-0.5">{item}</th>
