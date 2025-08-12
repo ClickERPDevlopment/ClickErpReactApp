1666971c94d62236017ac3a33a8a514b2a890692
mamun <mamun@clickerp.com.bd>
2025-08-12 14:19:14 +0600

    AtoZReportIndex

--- src/reports/merchandising/report/a-to-z-report/a-to-z-report-index.tsx
+++ src/reports/merchandising/report/a-to-z-report/a-to-z-report-index.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router";
-import axios from "axios";
 
 import useApiUrl from "../../../../hooks/use-ApiUrl";
 import TableSkeleton from "../../../../components/table-skeleton";
@@ -9,11 +8,17 @@ import { IAtoZReport } from "./components/IAtoZReport";
 import TotalRow from "./components/total-row";
 import BuyerGroupSection from "./components/buyer-group-section";
 import { Skeleton } from "@/components/ui/skeleton";
+import AppAlert from "@/components/app-alert";
+import useAxiosInstance from "@/lib/axios-instance";
 
 export default function AtoZReportIndex() {
   const [data, setData] = useState<IAtoZReport[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [isRendering, setIsRendering] = useState(true);
   const [searchParams] = useSearchParams();
+  const [error, setError] = useState('');
+  const axios = useAxiosInstance();
+
 
   let isOpmWise: boolean = false;
   let fromOpmDate: string | null = "";
@@ -62,15 +67,23 @@ export default function AtoZReportIndex() {
           )
           .then((res) => {
             if (res.data) {
-              setTimeout(() => {
-                setData(res.data);
-                setIsLoading(false);
-              }, 1000);
+              setData(res.data);
+              requestAnimationFrame(() => {
+                setIsRendering(false);
+              });
+              setIsLoading(false);
             } else {
-              console.log(res);
+              console.log('error', res);
+              setError('Can not load data. Please try again later.');
             }
           })
-          .catch((m) => console.log(m));
+          .catch((m) => {
+            setError('Can not load data. Please try again later.');
+            console.log('error', m)
+            setIsLoading(false);
+            setIsRendering(false);
+          }
+          );
 
         setIsLoading(false);
       } catch {
@@ -84,7 +97,7 @@ export default function AtoZReportIndex() {
 
   return (
     <>
-      {isLoading ? (
+      {isLoading || isRendering ? (
         <>
           <h3 className=" text-center p-1 m-4 text-3xl font-bold ">
             <Skeleton className="min-h-10 min-w-64" />
@@ -92,79 +105,80 @@ export default function AtoZReportIndex() {
           <TableSkeleton />
         </>
       ) : (
-        <div className="">
+        error ? <AppAlert message={error} type="error" /> :
           <div className="">
-            {/* <h1 className="text-center font-bold text-2xl">A-Z Report</h1>
+            <div className="">
+              {/* <h1 className="text-center font-bold text-2xl">A-Z Report</h1>
               <h5 className="text-center font-bold text-base">Order Placement Month from {moment(fromOpmDate).format("MMM-YYYY")} to {moment(toOpmDate).format("MMM-YYYY")}</h5> */}
+            </div>
+
+            <div className="border border-gray-500 rounded-md my-5 max-h-screen/20 m-1">
+              <table className="w-full">
+                <thead className="sticky top-0 z-10">
+                  <tr>
+                    <th className="min-w-[100px]  text-balance  text-center p-1 rounded-tl-md border-r border-gray-500" rowSpan={2}>
+                      Buyer
+                    </th>
+                    <th className="min-w-[100px]  text-balance  text-center p-1 border-r border-gray-500 " rowSpan={2}>
+                      Order/Job #
+                    </th>
+                    <th className="min-w-[100px] text-balance  text-center p-1  border-r border-gray-500" rowSpan={2}>
+                      Style
+                    </th>
+                    <th className="min-w-[70px] text-balance text-center p-1 border-r border-gray-500 " rowSpan={2}>
+                      Qrder Qty [A]
+                    </th>
+                    <th className="text-balance text-center p-1 border-r border-gray-500 " rowSpan={2}>Consumption release Dt. </th>
+                    <th className="text-balance text-center p-1 border-r border-b border-gray-500 " colSpan={6}>Yarn</th>
+                    <th className="text-balance text-center p-1 border-r border-gray-500 " rowSpan={2}>Yarn Issue Last.Dt.</th>
+                    <th className="text-balance text-center p-1 border-r border-gray-500 " rowSpan={2}>Knitting Start Dt.</th>
+                    <th className="text-balance text-center p-1 border-r border-b border-gray-500 " colSpan={3}>Knitting</th>
+                    <th className="text-balance text-center p-1 border-r border-gray-500 " rowSpan={2}>Knitting Close Dt.</th>
+                    <th className="text-balance text-center p-1 border-r border-b border-gray-500 " colSpan={7}>Dyeing</th>
+                    <th className="text-balance text-center p-1 border-r border-b border-gray-500 " colSpan={3}>Finish fabric</th>
+                    <th className="text-balance text-center p-1 border-r border-gray-500 " rowSpan={2}>Fin Fabrics Del. Last Date</th>
+                  </tr>
+                  <tr>
+                    {/* <th colSpan={6}>Yarn</th> */}
+
+                    <th className="text-balance text-center p-1 border-r border-gray-500  ">Required (KG) [B]	</th>
+                    <th className="text-balance text-center p-1 border-r border-gray-500  ">Allocation [C]	</th>
+                    <th className="text-balance text-center p-1 border-r border-gray-500  ">Bal <span className="text-nowrap">[D=B-C]</span>	</th>
+                    <th className="text-balance text-center p-1 border-r border-gray-500  ">Yarn Allocation Close Dt.	</th>
+                    <th className="text-balance text-center p-1 border-r border-gray-500  ">Issue [E]	</th>
+                    <th className="text-balance text-center p-1 border-r border-gray-500  ">Issue Bal <span className="text-nowrap">[F=C-E] </span></th>
+
+
+                    {/* <th colSpan={3}>Knitting</th> */}
+                    <th className="text-balance text-center p-1 border-r border-gray-500  ">Knitting Qty [G]	<span className="hidden">(Grey rcv by gmt) </span></th>
+                    <th className="text-balance text-center p-1 border-r border-gray-500  "> Knitting Bal <span className="text-nowrap">[H=B-G]</span></th>
+                    <th className="text-balance text-center p-1 border-r border-gray-500  "> WIP <span className="text-nowrap">[I=E-G]</span></th>
+
+
+                    {/* <th colSpan={7}>Dyeing</th> */}
+                    <th className="text-balance text-center p-1 border-r border-gray-500  ">Gray rcvd [J]	</th>
+                    <th className="text-balance text-center p-1 border-r border-gray-500  ">Batch Qty [K]	</th>
+                    <th className="text-balance text-center p-1 border-r border-gray-500  ">Ready for batch <span className="text-nowrap">[L=J-K]</span> 	</th>
+                    <th className="text-balance text-center p-1 border-r border-gray-500  ">Dyeing Qty [M]	</th>
+                    <th className="text-balance text-center p-1 border-r border-gray-500">Finishing Qty [N]	</th>
+                    <th className="text-balance text-center p-1 border-r border-gray-500">Deliver Qty [O]	</th>
+                    <th className="text-balance text-center p-1 border-r border-gray-500">RFT <span className="text-nowrap">[P=N-O]</span> 	</th>
+
+                    {/* <th colSpan={3}>Finish fabric</th> */}
+                    <th className="text-balance text-center p-1 border-r border-gray-500">Req Qty<span className="text-nowrap">[Q=B*0.89]</span> 	</th>
+                    <th className="text-balance text-center p-1 border-r border-gray-500">Rcvd Qty [R]	</th>
+                    <th className="text-balance text-center p-1  border-r border-gray-500">Revd Bal <span className="text-nowrap">[S=Q-R]</span> </th>
+                  </tr>
+                </thead>
+                <tbody id="table-body">
+                  {uniqueBuyer?.map((x, i) => (
+                    <BuyerGroupSection data={data?.filter(y => y.BUYER === x)} key={i} />
+                  ))}
+                  <TotalRow data={data} title="Total" />
+                </tbody>
+              </table>
+            </div>
           </div>
-
-          <div className="border border-gray-500 rounded-md my-5 max-h-screen/20 m-1">
-            <table className="w-full">
-              <thead className="sticky top-0 z-10">
-                <tr>
-                  <th className="min-w-[100px]  text-balance  text-center p-1 rounded-tl-md border-r border-gray-500" rowSpan={2}>
-                    Buyer
-                  </th>
-                  <th className="min-w-[100px]  text-balance  text-center p-1 border-r border-gray-500 " rowSpan={2}>
-                    Order/Job #
-                  </th>
-                  <th className="min-w-[100px] text-balance  text-center p-1  border-r border-gray-500" rowSpan={2}>
-                    Style
-                  </th>
-                  <th className="min-w-[70px] text-balance text-center p-1 border-r border-gray-500 " rowSpan={2}>
-                    Qrder Qty [A]
-                  </th>
-                  <th className="text-balance text-center p-1 border-r border-gray-500 " rowSpan={2}>Consumption release Dt. </th>
-                  <th className="text-balance text-center p-1 border-r border-b border-gray-500 " colSpan={6}>Yarn</th>
-                  <th className="text-balance text-center p-1 border-r border-gray-500 " rowSpan={2}>Yarn Issue Last.Dt.</th>
-                  <th className="text-balance text-center p-1 border-r border-gray-500 " rowSpan={2}>Knitting Start Dt.</th>
-                  <th className="text-balance text-center p-1 border-r border-b border-gray-500 " colSpan={3}>Knitting</th>
-                  <th className="text-balance text-center p-1 border-r border-gray-500 " rowSpan={2}>Knitting Close Dt.</th>
-                  <th className="text-balance text-center p-1 border-r border-b border-gray-500 " colSpan={7}>Dyeing</th>
-                  <th className="text-balance text-center p-1 border-r border-b border-gray-500 " colSpan={3}>Finish fabric</th>
-                  <th className="text-balance text-center p-1 border-r border-gray-500 " rowSpan={2}>Fin Fabrics Del. Last Date</th>
-                </tr>
-                <tr>
-                  {/* <th colSpan={6}>Yarn</th> */}
-
-                  <th className="text-balance text-center p-1 border-r border-gray-500  ">Required (KG) [B]	</th>
-                  <th className="text-balance text-center p-1 border-r border-gray-500  ">Allocation [C]	</th>
-                  <th className="text-balance text-center p-1 border-r border-gray-500  ">Bal <span className="text-nowrap">[D=B-C]</span>	</th>
-                  <th className="text-balance text-center p-1 border-r border-gray-500  ">Yarn Allocation Close Dt.	</th>
-                  <th className="text-balance text-center p-1 border-r border-gray-500  ">Issue [E]	</th>
-                  <th className="text-balance text-center p-1 border-r border-gray-500  ">Issue Bal <span className="text-nowrap">[F=C-E] </span></th>
-
-
-                  {/* <th colSpan={3}>Knitting</th> */}
-                  <th className="text-balance text-center p-1 border-r border-gray-500  ">Knitting Qty [G]	<span className="hidden">(Grey rcv by gmt) </span></th>
-                  <th className="text-balance text-center p-1 border-r border-gray-500  "> Knitting Bal <span className="text-nowrap">[H=B-G]</span></th>
-                  <th className="text-balance text-center p-1 border-r border-gray-500  "> WIP <span className="text-nowrap">[I=E-G]</span></th>
-
-
-                  {/* <th colSpan={7}>Dyeing</th> */}
-                  <th className="text-balance text-center p-1 border-r border-gray-500  ">Gray rcvd [J]	</th>
-                  <th className="text-balance text-center p-1 border-r border-gray-500  ">Batch Qty [K]	</th>
-                  <th className="text-balance text-center p-1 border-r border-gray-500  ">Ready for batch <span className="text-nowrap">[L=J-K]</span> 	</th>
-                  <th className="text-balance text-center p-1 border-r border-gray-500  ">Dyeing Qty [M]	</th>
-                  <th className="text-balance text-center p-1 border-r border-gray-500">Finishing Qty [N]	</th>
-                  <th className="text-balance text-center p-1 border-r border-gray-500">Deliver Qty [O]	</th>
-                  <th className="text-balance text-center p-1 border-r border-gray-500">RFT <span className="text-nowrap">[P=N-O]</span> 	</th>
-
-                  {/* <th colSpan={3}>Finish fabric</th> */}
-                  <th className="text-balance text-center p-1 border-r border-gray-500">Req Qty<span className="text-nowrap">[Q=B*0.89]</span> 	</th>
-                  <th className="text-balance text-center p-1 border-r border-gray-500">Rcvd Qty [R]	</th>
-                  <th className="text-balance text-center p-1  border-r border-gray-500">Revd Bal <span className="text-nowrap">[S=Q-R]</span> </th>
-                </tr>
-              </thead>
-              <tbody id="table-body">
-                {uniqueBuyer?.map((x, i) => (
-                  <BuyerGroupSection data={data?.filter(y => y.BUYER === x)} key={i} />
-                ))}
-                <TotalRow data={data} title="Total" />
-              </tbody>
-            </table>
-          </div>
-        </div>
       )}
     </>
   );
