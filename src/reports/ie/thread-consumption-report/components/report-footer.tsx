9b0492f8497bda85c82f89398c4ee154c52886a3
raihan50017 <aburaihan50017@gmail.com>
2025-08-19 12:46:58 +0600

    Thread Consumption Sheet Update

--- src/reports/ie/thread-consumption-report/components/report-footer.tsx
+++ src/reports/ie/thread-consumption-report/components/report-footer.tsx
@@ -13,8 +13,8 @@ function ReportFooter({ }: { data: ThreadConsumptionReportType[] }) {
           <tr>
             {/* <td><span className='pb-1 border-t border-gray-900 px-2'>Line Chief</span></td> */}
             {/* <td><span className='pb-1 border-t border-gray-900 px-2'>Maintenance</span></td> */}
-            <td className="text-center"><span className='pb-1 border-t border-gray-900 px-2'>IE</span></td>
-            <td className="text-center"><span className='pb-1 border-t border-gray-900 px-2'>Head of IE</span></td>
+            <td className="text-start"><span className='pb-1 border-t border-gray-900 px-2'>Sr. Executive (IE)</span></td>
+            <td className="text-end"><span className='pb-1 border-t border-gray-900 px-2'>Head Of IE</span></td>
             {/* <td><span className='pb-1 border-t border-gray-900 px-2'>AGM/GM</span></td> */}
             {/* <td><span className='pb-1 border-t border-gray-900 px-2'>Planning</span></td> */}
           </tr>
