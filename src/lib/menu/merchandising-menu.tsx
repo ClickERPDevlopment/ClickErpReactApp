64d59add36917cd885a391a4f07522589e49be8f
mamun <mamun166036@yahoo.com>
2025-09-18 22:52:39 +0600

    up

--- src/lib/menu/merchandising-menu.tsx
+++ src/lib/menu/merchandising-menu.tsx
@@ -8,28 +8,37 @@ export const MerchandisingMenu: MenuItem[] = [
     dirchange: false,
   },
   {
+    path: "/dashboard",
+    title: "Home",
     icon: <i className="side-menu__icon bx bx-home"></i>,
-    type: "sub",
-    Name: "",
-    active: false,
+    type: "link",
     selected: false,
     dirchange: false,
-    title: "Dashboards",
-    badge: "",
-    badgetxt: "12",
-    class:
-      "badge !bg-warning/10 !text-warning !py-[0.25rem] !px-[0.45rem] !text-[0.75em] ms-2",
-    children: [
-      {
-        path: "/dashboard",
-        type: "link",
-        active: false,
-        selected: false,
-        dirchange: false,
-        title: "Home",
-      },
-    ],
+    active: false,
   },
+  // {
+  //   icon: <i className="side-menu__icon bx bx-home"></i>,
+  //   type: "sub",
+  //   Name: "",
+  //   active: false,
+  //   selected: false,
+  //   dirchange: false,
+  //   title: "Dashboards",
+  //   badge: "",
+  //   badgetxt: "12",
+  //   class:
+  //     "badge !bg-warning/10 !text-warning !py-[0.25rem] !px-[0.45rem] !text-[0.75em] ms-2",
+  //   children: [
+  //     {
+  //       path: "/dashboard",
+  //       type: "link",
+  //       active: false,
+  //       selected: false,
+  //       dirchange: false,
+  //       title: "Home",
+  //     },
+  //   ],
+  // },
 
   {
     menutitle: "WEB APPS",
