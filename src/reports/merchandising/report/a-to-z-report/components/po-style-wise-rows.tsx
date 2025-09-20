fca60bba67dd27fc8a3541c72d66840afc45d9d9
mamun <mamun@clickerp.com.bd>
2025-09-20 17:13:17 +0600

    up

--- src/reports/merchandising/report/a-to-z-report/components/po-style-wise-rows.tsx
+++ src/reports/merchandising/report/a-to-z-report/components/po-style-wise-rows.tsx
@@ -33,14 +33,7 @@ export default function PoStyleWiseRows({ data_fabric, data_gmt, poStyleIndex }:
                             </td>
                             <td className="border border-gray-500 text-center p-1 text-nowrap" rowSpan={maxLength}>
                                 <span className="hidden">{f?.PO_ID}</span>
-                                <a
-                                    href={`/report/embellishment/embellishment-send-receive-report?fromOpmDate=01-Jan-2025&toOpmDate=20-Dec-2025&fromSendRcvDate=20-Sep-2025&toSendRcvDate=20-Sep-2025&isOpmDate=false&isSendRcvDate=False&buyerId=0&styleId=0&poId=${f?.PO_ID}&colorId=0&companyId=0&loggedInCompanyId=1`} // your target route
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                    className="text-sky-600 hover:underline cursor-pointer"
-                                >
-                                    {f?.PONO}
-                                </a>
+                                {f?.PONO}
                             </td>
                             <td className="border border-gray-500 text-nowrap text-center p-1" rowSpan={maxLength}>
                                 <span className="hidden">{f?.STYLE_ID}</span>
@@ -96,7 +89,16 @@ export default function PoStyleWiseRows({ data_fabric, data_gmt, poStyleIndex }:
                                     moment(f?.FIN_FABRICS_DEL_LAST_DATE).format("DD-MMM-YYYY")}
                             </td>
                             {/* gmt-General Info */}
-                            <td className="border border-gray-500 text-balance text-center p-1 border-l">{g?.PONO}</td>
+                            <td className="border border-gray-500 text-balance text-center p-1 border-l">
+                                <a
+                                    href={`/report/embellishment/embellishment-send-receive-report?fromOpmDate=01-Jan-2025&toOpmDate=20-Dec-2025&fromSendRcvDate=20-Sep-2025&toSendRcvDate=20-Sep-2025&isOpmDate=false&isSendRcvDate=False&buyerId=0&styleId=0&poId=${g?.PO_ID}&colorId=0&companyId=0&loggedInCompanyId=1`} // your target route
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="text-sky-600 hover:underline cursor-pointer"
+                                >
+                                    {g?.PONO}
+                                </a>
+                            </td>
                             <td className="border border-gray-500 text-nowrap text-center p-1">{g?.STYLENO}</td>
                             <td className="border border-gray-500 text-nowrap text-center p-1">{g?.LINENAME}</td>
                             <td className="border border-gray-500 text-nowrap text-center p-1">{g?.PO_QTY}</td>
@@ -136,7 +138,16 @@ export default function PoStyleWiseRows({ data_fabric, data_gmt, poStyleIndex }:
                     return (
                         <tr className={cn("border-t border-gray-500")} key={i}>
                             {/* gmt-General Info */}
-                            <td className="border border-gray-500 text-balance text-center p-1">{g?.PONO}</td>
+                            <td className="border border-gray-500 text-balance text-center p-1">
+                                <a
+                                    href={`/report/embellishment/embellishment-send-receive-report?fromOpmDate=01-Jan-2025&toOpmDate=20-Dec-2025&fromSendRcvDate=20-Sep-2025&toSendRcvDate=20-Sep-2025&isOpmDate=false&isSendRcvDate=False&buyerId=0&styleId=0&poId=${g?.PO_ID}&colorId=0&companyId=0&loggedInCompanyId=1`} // your target route
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="text-sky-600 hover:underline cursor-pointer"
+                                >
+                                    {g?.PONO}
+                                </a>
+                            </td>
                             <td className="border border-gray-500 text-nowrap text-center p-1">{g?.STYLENO}</td>
                             <td className="border border-gray-500 text-nowrap text-center p-1">{g?.LINENAME}</td>
                             <td className="border border-gray-500 text-nowrap text-center p-1">{g?.PO_QTY}</td>
