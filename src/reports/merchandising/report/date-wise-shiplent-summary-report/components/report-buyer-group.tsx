4a82a6a759e5e874e6d163434b1842c543d6061c
raihan50017 <aburaihan50017@gmail.com>
2025-09-22 15:25:09 +0600

    DateWiseShiplentSummaryReport Update

---
+++ src/reports/merchandising/report/date-wise-shiplent-summary-report/components/report-buyer-group.tsx
@@ -0,0 +1,48 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import moment from "moment";
+import { DateWiseShiplentSummaryReportType } from "../date-wise-shiplent-summary-report-type";
+
+function ReportBuyerGroup({
+  data,
+}: {
+  data: DateWiseShiplentSummaryReportType[];
+}) {
+
+  const totalOrderQty = data.reduce((sum, item) => sum + item.ORDER_QTY, 0);
+  const totalShipmentQty = data.reduce((sum, item) => sum + item.SHIPMENTQTY, 0);
+  const totalValue = data.reduce((sum, item) => sum + (item.SHIPMENTQTY * item.FOB), 0);
+
+  return (
+    <>
+      {data.map((item) => (
+        <tr className="text-center">
+          <td className="border border-gray-300 p-1">
+            {moment(item.CHALLANDATE).format("DD-MMM-YY")}
+          </td>
+          <td className="border border-gray-300 p-1 text-start">
+            {item.BUYER}
+          </td>
+          <td className="border border-gray-300 p-1 text-start">{item.STYLENO}</td>
+          <td className="border border-gray-300 p-1 text-start">{item.STYLENAME}</td>
+          <td className="border border-gray-300 p-1 text-start">{item.PONO}</td>
+          <td className="border border-gray-300 p-1 text-right">{item.ORDER_QTY}</td>
+          <td className="border border-gray-300 p-1 text-right">{item.PREV_SHIPMENTQTY}</td>
+          <td className="border border-gray-300 p-1 text-right">{(item.ORDER_QTY - item.PREV_SHIPMENTQTY - item.SHIPMENTQTY)}</td>
+          <td className="border border-gray-300 p-1 text-right">{item.SHIPMENTQTY}</td>
+          <td className="border border-gray-300 p-1 text-right">{(item.SHIPMENTQTY * item.FOB).toFixed(2)}</td>
+        </tr>
+      ))}
+
+      <tr className="text-center font-bold bg-emerald-100">
+        <td colSpan={5} className="border border-gray-300 p-1 text-right">Buyer Wise Total</td>
+        <td className="border border-gray-300 p-1 text-right">{totalOrderQty}</td>
+        <td className="border border-gray-300 p-1">{ }</td>
+        <td className="border border-gray-300 p-1">{ }</td>
+        <td className="border border-gray-300 p-1 text-right">{totalShipmentQty}</td>
+        <td className="border border-gray-300 p-1 text-right">{totalValue.toFixed(2)}</td>
+      </tr>
+    </>
+  );
+}
+
+export default ReportBuyerGroup;
---
+++ src/reports/merchandising/report/date-wise-shiplent-summary-report/date-wise-shiplent-summary-report-type.ts
@@ -0,0 +1,16 @@
+export type DateWiseShiplentSummaryReportType = {
+  FACTORYID?: string;
+  CHALLANDATE: string;
+  BUYERID?: number;
+  STYLE_ID?: number;
+  PONO?: string;
+  ORDER_QTY: number;
+  FOB: number;
+  COMPANY_NAME?: string;
+  COMPANY_ADDRESS?: string;
+  BUYER?: string;
+  STYLENAME?: string;
+  STYLENO?: string;
+  SHIPMENTQTY: number;
+  PREV_SHIPMENTQTY: number;
+};
