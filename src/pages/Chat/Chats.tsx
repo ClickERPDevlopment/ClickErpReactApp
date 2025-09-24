8144b81bf3e2653c5b9e17a5408baa4ec0dcec3c
mamun <mamun@clickerp.com.bd>
2025-09-24 15:51:09 +0600

    up

--- src/pages/Chat/Chats.tsx
+++ src/pages/Chat/Chats.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Input } from "@/components/ui/input";
 import UserCard from "./UserCard";
 import { useNotifications } from "@/utility/NotificationProvider";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export default function Chats({ selectedUser, setSelectedUser }: { selectedUser: string, setSelectedUser: React.Dispatch<React.SetStateAction<string>> }) {
     const { reactUser } = useNotifications();
@@ -17,11 +18,15 @@ export default function Chats({ selectedUser, setSelectedUser }: { selectedUser:
             </div>
 
             {/* Messages Area - Fixed height with proper scrolling */}
-            <div className="overflow-y-scroll flex flex-col flex-1 items-center p-2 rounded-bl-md border-b border-b-gray-300 w-full gap-1">
-                <UserCard userName="All" lastMsg="" selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
-                {reactUser?.map(item =>
-                    <UserCard userName={item.UserName} lastMsg="" selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
-                )}
+            <div className="h-[calc(100vh-210px)] px-1">
+                <ScrollArea className="h-full w-full">
+                    <div className="flex flex-col flex-1 items-center rounded-bl-md w-full gap-1">
+                        <UserCard userName="All" lastMsg="" selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+                        {reactUser?.map(item =>
+                            <UserCard userName={item.UserName} lastMsg="" selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+                        )}
+                    </div>
+                </ScrollArea>
             </div>
         </div>
     );
