8a55188b7bbf8f1d02f140c4d4ed0d194fc6b65e
raihan50017 <aburaihan50017@gmail.com>
2025-12-10 10:42:04 +0600

    Compensation Claim Entry Page Update

--- src/reports/merchandising/report/compensation-claim-report/components/report.tsx
+++ src/reports/merchandising/report/compensation-claim-report/components/report.tsx
@@ -30,7 +30,7 @@ function Report({
   // );
 
   const totalClaimAmount = data?.ClaimDetails?.reduce(
-    (acc, item) => acc + item.CLAIM_AMOUNT,
+    (acc, item) => acc + (item.CLAIM_AMOUNT_PER_UNIT * item.QUANTITY_DAMAGED),
     0
   );
 
@@ -136,7 +136,7 @@ function Report({
               <td className="border border-gray-950">{item.QUANTITY_DAMAGED}</td>
               <td className="border border-gray-950">{item.UOM}</td>
               <td className="border border-gray-950">{item.CLAIM_AMOUNT_PER_UNIT}</td>
-              <td className="border border-gray-950">{item.CLAIM_AMOUNT}</td>
+              <td className="border border-gray-950">{(item.CLAIM_AMOUNT_PER_UNIT * item.QUANTITY_DAMAGED).toFixed(2)}</td>
               <td className="border border-gray-950 text-left px-2">{item.DAMAGE_DETAILS}</td>
               <td className="border border-gray-950 text-left px-2">{item.ACTION_TAKEN}</td>
             </tr>
@@ -150,7 +150,7 @@ function Report({
             <td className="border border-gray-950">{ }</td>
             <td className="border border-gray-950"></td>
             <td className="border border-gray-950"></td>
-            <td className="border border-gray-950">{totalClaimAmount}</td>
+            <td className="border border-gray-950">{totalClaimAmount?.toFixed(2)}</td>
             <td className="border border-gray-950" colSpan={3}></td>
           </tr>
         </tbody>
